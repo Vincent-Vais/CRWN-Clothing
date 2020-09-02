c0d9f712c63c439eb9b3a6e5f2644a65e84ba08b
Vincent-Vais <vincentvais17@gmail.com>
2020-09-02 17:49:16 -0500

    added redux for existing components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,10 @@
-import React from "react";
 import "./header.styles.scss";
 
+import React from "react";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
-
 import { Link } from "react-router-dom";
 import { auth } from "../../firebase/firebase.utils";
+import { connect } from "react-redux";
 
 const Header = ({ currentUser }) => (
   <div className="header">
@@ -33,4 +33,6 @@ const Header = ({ currentUser }) => (
   </div>
 );
 
-export default Header;
+const mapStateToProps = (state) => ({ currentUser: state.user.currentUser });
+
+export default connect(mapStateToProps)(Header);
---
+++ src/redux/root-reducer.js
@@ -0,0 +1,5 @@
+import userReducer from "./user/user.reducer";
+
+import { combineReducers } from "redux";
+
+export default combineReducers({ user: userReducer });
